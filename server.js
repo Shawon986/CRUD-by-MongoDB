41e3cf9a0f47c3352d0d3bb4905c9177482c8385
Shawon Hossain <mdmonowerhossain986@gmail.com>
2023-07-27 22:18:14 +0600

    mongodb connection done

--- server.js
+++ server.js
@@ -1,14 +1,27 @@
 const express = require("express")
 const bodyParser = require("body-parser")
 const dotenv = require("dotenv").config()
+const mongoose = require("mongoose")
 const app = express()
 app.use(bodyParser.json())
+
+//! MongoDb connection
+const uri = process.env.DB_URI
+mongoose.connect(uri,{useNewUrlParser:true})
+.then(()=>{
+    console.log("DB is connected")
+})
+.catch((error)=>{
+    console.error("DB is not connected")
+})
+
 
 
 //! Connection Check
 app.get("/",(req,res)=>{
     res.json({message:"Welcome to Crud app"})
 })
+
 
 
 const port = process.env.PORT 

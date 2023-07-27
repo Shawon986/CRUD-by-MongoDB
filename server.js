831a420861382a7f67da9b2083b7da774784096b
Shawon Hossain <mdmonowerhossain986@gmail.com>
2023-07-27 22:31:03 +0600

    create visitor api done

--- server.js
+++ server.js
@@ -36,6 +36,25 @@ app.get("/",(req,res)=>{
     res.json({message:"Welcome to Crud app"})
 })
 
+//! Create Visitor
+app.post("/visitors",async(req,res)=>{
+    try {
+        const visitorObject ={
+            name:req.body.name,
+            email:req.body.email,
+            password:req.body.password
+        }
+        const visitor = new Visitors(visitorObject)
+        res.status(201).json(visitor)
+        await visitor.save()
+    } catch (error) {
+        console.error(error)
+        res.status(400).json({message:"Something went wrong with the server !!!"})
+    }
+    
+
+})
+
 
 
 const port = process.env.PORT 
